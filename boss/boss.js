5a2dfb720391af1141c0192fcbb246a8536e077b
gzwgq222 <445722156@qq.com>
2019-11-28 18:35:31 +0800

    feat: 爬取 狗带TV 电影资源

--- boss/boss.js
+++ boss/boss.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer');
 
-// const url = 'https://www.zhipin.com/c101280600-p100901/';
-const url = 'https://www.zhipin.com/?sid=sem_pz_bdpc_dasou_title';
+const url = 'https://www.zhipin.com/c101280600-p100901/';
+// const url = 'https://www.zhipin.com/?sid=sem_pz_bdpc_dasou_title';
 // const url = 'https://www.baidu.com/';
 const config = {
     slowMo: 200,
+    headless: false,
     devtools: true,
     timeout: 0,
+    args: ['--no-sandbox'],
     defaultViewport: {
         width: 1376,
         height: 768,
@@ -15,21 +17,22 @@ const config = {
 
 puppeteer.launch(config).then(async browser => {
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36');
+    await page.goto(url, {timeout: 0});
     page.on('console', msg => console.log('PAGE LOG:', msg.text()));
     // const title = await page.title();
     // await page.type('#kw', 'puppeteer');
     // await page.keyboard.press('Enter');
     // const title = await page.title();
 
-    const BRANDS_SELECTOR = '.job-menu';
-    const brands = await page.evaluate(sel => {
-        const ulList = Array.from($(sel).find('dl dd a'));
-        console.log(888);
-        const web = ulList.find(item => item.innerText.includes('前端'));
-        console.log(web.innerText);
-        return web.innerText;
-    }, BRANDS_SELECTOR);
-    console.log(brands);
+    // const BRANDS_SELECTOR = '.job-menu';
+    // const brands = await page.evaluate(sel => {
+    //     const ulList = Array.from($(sel).find('dl dd a'));
+    //     console.log(888);
+    //     const web = ulList.find(item => item.innerText.includes('前端'));
+    //     console.log(web.innerText);
+    //     return web.innerText;
+    // }, BRANDS_SELECTOR);
+    // console.log(brands);
     // await page.click('#main > div > div.home-sider > div > dl:nth-child(1) > dd > a:nth-child(5)');
 });
--- movie/index.js
+++ movie/index.js
@@ -2,38 +2,80 @@ const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 
-const jsonUrl = path.join(__dirname, 'move.json');
-const stream = fs.createWriteStream(jsonUrl);
 // stream.write("888", 'utf8');
 
 // console.log(__dirname)
 // console.log(__filename)
 // console.log(process.cwd())
+const tm = {
+    timeout: 0
+};
 const config = {
-    headless: false,
-    timeout: 0,
-    defaultViewport: {
-        width: 1376,
-        height: 768,
-    }
+    ...tm,
+    // headless: false,
+    // defaultViewport: {
+    //     width: 1376,
+    //     height: 768,
+    // }
+};
 
-}
-const url = 'http://www.xiguayyv1.com/';
+const url = 'http://www.xiguayyv1.com';
+
+console.log('---启动浏览器---');
 puppeteer.launch(config).then(async browser => {
+    console.log('---打开tab---');
     const page = await browser.newPage();
+    console.log('---跳转页面---');
     await page.goto(url);
-    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
-    const movie = '.stui-vodlist.clearfix'
-    // await page.click('body > div:nth-child(4) > div > div:nth-child(1) > div > div.stui-pannel_bd.clearfix > ul > li:nth-child(1) > div');
+    page.on('console', msg => console.log('LOG:', msg.text()));
+
+    // 首页
+    console.log('---首页---');
+    const movie = 'body > div:nth-child(4) > div > div:nth-child(1) > div > div.stui-pannel_bd.clearfix > ul';
     const res = await page.evaluate( sel => {
-        const ulList = Array.from($(sel).find('h4 a'));
-        const data = ulList.map(item => {
-            console.log(item.innerText);
-            return item.innerText;
-        })
-        // stream.write(JSON.stringify(brands, undefined, 2), 'UTF8');
-        console.log(2, ulList.length);
-        return data;
-    }, movie)
-    stream.write(JSON.stringify(res, undefined, 2), 'utf8');
+        const ulList = Array.from($(sel).find('a.stui-vodlist__thumb.lazyload'));
+        return ulList.map(item => item.getAttribute('href'));
+    }, movie);
+    console.log('movie: ', res.length);
+
+    // 详情页
+    console.log('---详情页---');
+    const movieHtml = await Promise.all(res.map(async (item, index) => {
+        const moviePage = await browser.newPage();
+        await moviePage.goto(`${url}${item}`, tm);
+        const href = await moviePage.$eval(
+            'body > div:nth-child(7) > div > div.col-lg-wide-75.col-xs-1.padding-0 > div:nth-child(5) > div > div.stui-pannel_bd.col-pd.clearfix > ul > li > a',
+            el => el.href
+        );
+        moviePage.close();
+        return href;
+    }))
+
+    // 播放页
+    console.log('---播放页---');
+    const playLink = await Promise.all(movieHtml.map(async item => {
+        const playPage = await browser.newPage();
+        await playPage.goto(item, tm);
+        const src = await playPage.$eval('#Player', el => el.src);
+        const info = await playPage.$eval('.thumb > a', el => {
+            return {
+               title: el.title,
+               img: el.getAttribute('data-original')
+            }
+        });
+        playPage.close();
+        return {
+            ...info,
+            src: src.split('?url=')[1]
+        };
+    }));
+    console.log('结果：', playLink);
+
+    console.log('---写入流---');
+    const jsonUrl = path.join(__dirname, 'move.json');
+    const stream = fs.createWriteStream(jsonUrl);
+    stream.write(JSON.stringify(playLink, undefined, 2), 'utf8');
+    await page.close();
+    await browser.close();
+    console.log('爬取完成');
 })
